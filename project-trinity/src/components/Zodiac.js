bead2e1f4cfdb55fe54386fd61a88a394cf1e9da
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-06-25 21:15:08 -0700

    Added Gemini and fixed desktop width card

--- project-trinity/src/components/Zodiac.js
+++ project-trinity/src/components/Zodiac.js
@@ -5,6 +5,8 @@ import ariesdesktop from '../assets/zodiacs/cards/aries-card-desktop.svg'
 import ariesCard from '../assets/zodiacs/cards/aries-card.svg'
 import taurusdesktop from '../assets/zodiacs/cards/taurus-card-desktop.svg'
 import taurusCard from '../assets/zodiacs/cards/taurus-card.svg'
+import geminidesktop from '../assets/zodiacs/cards/gemini-card-desktop.svg'
+import geminiCard from '../assets/zodiacs/cards/gemini-card.svg'
 import taurus from '../assets/zodiacs/taurus.svg'
 import gemini from '../assets/zodiacs/gemini.svg'
 import cancer from '../assets/zodiacs/cancer.svg'
@@ -31,6 +33,10 @@ function Zodiac() {
   // Function to handle Taurus click
   const handleTaurusClick = () => {
     setVisibleDiv(visibleDiv === 'Taurus' ? null : 'Taurus');
+  };
+   // Function to handle Gemini click
+   const handleGeminiClick = () => {
+    setVisibleDiv(visibleDiv === 'Gemini' ? null : 'Gemini');
   };
 
     return (
@@ -59,6 +65,17 @@ function Zodiac() {
 
         </motion.div>
       )}
+       {visibleDiv === 'Gemini' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={geminidesktop} alt="taurus description" />
+          <img className="card" src={geminiCard} alt="taurus description" />
+
+        </motion.div>
+      )}
      
       <div className='zodiac-wheel-container'>
         
@@ -68,7 +85,7 @@ function Zodiac() {
           class="zodiac-wheel">
           <div onClick={handleAriesClick} class="slice aries"><span><img  src={aries} alt="aries"/></span></div>
           <div onClick={handleTaurusClick} class="slice taurus"><span><img src={taurus} alt="taurus"/></span></div>
-          <div class="slice gemini"><span><img src={gemini} alt="gemini"/></span></div>
+          <div onClick={handleGeminiClick}class="slice gemini"><span><img src={gemini} alt="gemini"/></span></div>
           <div class="slice cancer"><span><img src={cancer} alt="cancer"/></span></div>
           <div class="slice leo"><span><img src={leo} alt="leo"/></span></div>
           <div class="slice virgo"><span><img src={virgo} alt="virgo"/></span></div>
