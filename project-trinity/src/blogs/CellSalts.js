dd9b56bed4b32bfc28e8ba0d7db82fdaf8660a11
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-06-29 07:34:38 -0700

    Few

--- project-trinity/src/blogs/CellSalts.js
+++ project-trinity/src/blogs/CellSalts.js
@@ -66,7 +66,7 @@ function CellSalts() {
                             </p>
                             
                             <p>
-                                This is based on the work done by 
+                                This wheel is based on the work done by 
                                 Dr. George W. Carey, a famous biochemist and astrologer 
                                 from the 1900s. His work and discoveries will help 
                                 mankind free itself from ignorance, and finally place 
@@ -79,194 +79,27 @@ function CellSalts() {
                                 where everything else has been manipulated and polluted. 
 
                             </p>
-                            <p>
-                                I am often criticized, misunderstood, and rejected when it comes to 
-                                my teachings on Astrotheology and how 'As Above, So Below' could 
-                                not be more true. I do not care about man judging me, for man does not 
-                                even know himself. How can I except man to fathom what has existed 
-                                as long as this barren earth. He cannot do it alone. I am not here to help, 
-                                nor I ever will be. I am here to save myself, and point out
-                                to others the very stone I have rejected. I repent, I know the truth is 
-                                understanding, mastering, and perfecting the human body, to achieve 
-                                Christ Consciousness and Regeneration. I know Jesus is king, he is the seed 
-                                born in the Solar Plexus, who is risen to the Pineal Gland every 
-                                28 1/2 days. He was not a person, he is a symbol, the symbol of the Celestial Man. 
-                                The perfect man. The new man. 
-                            </p>
-                            <p>Please select a Zodiac to read more:</p>
-                            <p>
-                               (Only Aries and Taurus work. I am still building the remaining Zodiac Cards)
-                            </p>
-                    
+                            <div className='quote'>
+                                <p>
+                                    “It must always be remembered that astrology is the consideration of the body of the universe and, by analogy, the anatomy, therefore, of man.” 
+                                </p>
+                                <p>
+                                    - Dr George W. Carey (1845 - 1924)
+                                </p>
+                            </div>
+                            <p>Please select a Zodiac to read a short summary:</p>
+                           
                             <Zodiac/>
 
                             
                             <h2>
-                                ♊️ Gemini - Kali Mur (Potassium Chloride)                            
-                            </h2>
-                           
-                            <p>
-                                Derived from Latin dis means “apart” and gerere “to carry”, 
-                                apart into different directions. Without Potassium Chloride, 
-                                no fibrins can be made. Definition of fiber is “an elongated, thread-like structure of organize tissue”. 
-                                With the exception of bones, it is evident that fibrin is necessary in the formation of the nerves, ligaments, 
-                                veins, skin, tissue—all flesh. 
-                            </p>
-                            <div className='quote'>
-                                <p>
-                                    “the spinning salt, the threads from which the seamless garment of flesh is woven”
-                                </p>
-                                <p>
-                                    - Dr. George W. Carey, The Cell Salts of Salvation
-                                </p>
-                            </div>
-                            <p>
-                                FOOD SOURCES: Peanuts, hazelnuts, lentils, potatoes, spinach, blackstrap molasses, ginger, coren, asparagus,
-                                celery, peaches, cauliflower, pineapple, kale, squash, red beets.
-                            </p>
-
-                            <h2>
-                                ♋️ Cancer - Calcium Flouride (Flouride of Lime)                           
-                            </h2>
-                            <p>
-                                Cancer pertains to fluids, and is one of the signs of water triplicity. Nature’s power to 
-                                reproduce the ceasless formation of corpuscles within the spleen where conception takes place. 
-                                Derived from latin, concipio eans to take (hold) together. 
-                                The corpuscle carries the spiritual germ or nucleus surrounded by fluid. 
-                            </p>
-                            <div className='quote'>
-                                <p>
-                                    “Give them their forms within. She will build coverings without. For Males-Females will they be. 
-                                    Lords of the flame, also.”
-                                </p>
-                                <p>
-                                    - Dr. George W. Carey, The Cell Salts of Salvation
-                                </p>
-                            </div>
-                            <p>
-                                FOOD SOURCES: Alfalfa, kelp, eggs, whole grains, raw green vegetables, beets, fish, apples, mushrooms, apples
-                            </p>
-                            <h2>
-                                ♌️ Leo - Magnesium Phosphate (Phosphate of Magnesium)                           
-                            </h2>
-                            <p>
-                                Derived from Latin, Leo means “to blot out, to destroy, to annihilate”, 
-                                keeping within the nature of this animal. As the heart is a great mass of 
-                                nervous muscular tissue concerned in dividing, lifting, and circulating the blod, 
-                                it recieves from the inner sun Aries, the divine fire. Energy generated in the inner 
-                                nucleus is wasted in emotion (motion thst moves out, goes forth from)
-                            </p>
-                            <div className='quote'>
-                                <p>
-                                    “As King of Beasts on Earth, is used as a symbol to illustrate the fact that energy or 
-                                    power may be used to give life (create) and take life (destroy)”
-                                </p>
-                                <p>
-                                    - Dr. George W. Carey, The Cell Salts of Salvation
-                                </p>
-                            </div>
-                            <p>
-                                FOOD SOURCES: Green vegetables, chlorophyll, chocolate (dark is best), nuts, seeds, whole grains, sea salt,
-                                corn, peas.
-                            </p>
-
-                            <h2>
-                                ♍️ Virgo - Kali Sulph (Sulphate of Potassium)
-                            </h2>                           
-                            <p>
-                                Latin for Virgin, Virgo is Bethulah in Hebrew meaning “house or place of pure water”. 
-                                To be pure, is to be perfect, unadulterated. Anything unadulterated means the original 
-                                elements have been separated, resulting in loss of cerebral esse to furnish the body 
-                                for an incoming ego. The indrawing of energy into the earth. It is the barren to carnality, 
-                                therefore represents barren earth, but it is indeed fertile to spirituality. Oil, 
-                                the physical manifestation of energy. 
-                            </p>
-                            <div className='quote'>
-                                <p>
-                                    “The Virgin: Symbol of the purified emotion, or purified lower nature, 
-                                    no longer fructified by the desire-mind”
-                                </p>
-                                <p>
-                                    - Dr. George W. Carey, The Cell Salts of Salvation
-                                </p>
-                            </div>
-                            <p>
-                                FOOD SOURCES: Green vegetables, chlorophyll, chocolate (dark is best), nuts, seeds, whole grains, sea salt,
-                                corn, peas.
-                            </p>
-                            <h2>
-                                ♎️ Libra - Nat Phos (Natrium of Phosphate)                            
-                            </h2>                           
-                            <p>
-                                The Balancing Salt which neutralizes acid. It changes acid to alkili by 
-                                releasing water from the tissues, collecting it in the kidneys, and carries 
-                                it to the bladder. Acid of any kind forms no part in human economy. Acid’s 
-                                original meaning is sharp and cutting which are the negatives of pleasant 
-                                and attractive. Therefore too scale the heights and gain the balance, Venus 
-                                must bid her son Cupid throw away his darts. 
-                            </p>
-                            <div className='quote'>
-                                <p>
-                                    “All Combat is ceaseth and it is santified. It is a symbol of 
-                                    the point of balance and change reached in the soul’s development
-                                     (sun’s course), when a line, as it were, is crossed and a new soul-process 
-                                     is entered upon”
-                                </p>
-                                <p>
-                                    - Dr. George W. Carey, The Cell Salts of Salvation
-                                </p>
-                            </div>
-                            <div className='quote'>
-                                <p>
-                                    “Balance, measure, and patience are the eternal conditions of high success”
-                                </p>
-                                <p>
-                                    - Matthew Arnol
-                                </p>
-                            </div>
-                            <p>
-                                FOOD SOURCES: Sea salt, red beets, lentils, radishes, tomatoes, dairy products, celery and celery seeds.
-                            </p>
-                            <h2>
-                                ♏︎ Scorpio - Calcium Sulphate (Sulphate of Lime)
-                            </h2>                            
-                            <p>
-                                Scorpio has been allocated with the organs of 
-                                procreation, and that their use is solely for procreation. It is termed 
-                                the VERY GATE OF HELL for it is the beginning of five dead cold months, 
-                                and the act of sexual excesses or self-abuse, not only actually impairs
-                                the activities of the five parts related to the remaining five zodiac 
-                                signs. The White Eagle is the emblematical of the Divine promise of 
-                                life everlasting to “him who overcometh.” Overcoming death, because
-                                death cannot take place where there are sufficient corpuscles to
-                                sustain life.
-                            </p>
-                            <div className='quote'>
-                                <p>
-                                    “Crab is actually changed into the scorpion. This means that 1/10s 
-                                    of the corpuscles of the body (all of which rightfully belong to the body)
-                                     under the vibration of the lower (carnal) mind, are attracted and drawn down 
-                                     into the testes, and are changed into the animal germs or corpuscles.
-                                    The truth is, if let alone (not lost), it will eventually go up. 
-                                    For out of the serpent’s root shall come forth a cockatrice, and his 
-                                    fruit shall be a fiery flying serpent”
-                                </p>
-                                <p>
-                                    - Dr. George W. Carey, The Cell Salts of Salvation
-                                </p>
-                            </div>
-                            <p>
-                                FOOD SOURCES: Oats, almonds, cucumbers, lentils, peanuts, soybeans, cauliflower, 
-                                onions, garlic, radishes, asparagus, celery, parsley, and green leafy vegetables.
-                            </p>
-
-                            <p>
-                                Remaining five zodiacs coming soon...
-                            </p>
-                           
-                            {/* <h2>
                                 Conclusion
                             </h2>
+                            <p>
+                                This is but a taste of the Cell Salts of Salvation by Dr George Carey. Sufficient to be understood, and 
+                                to be pursued with curiosity. When one embarks on this story in great detail, he will be left speechless. 
+                                As it is indeed, the unspoke wheel. 
+                            </p>
                            
                           
                            
@@ -275,10 +108,14 @@ function CellSalts() {
                             <h2>
                                 Post-morteum thoughts:
                             </h2>
+                            <p>
+                                I feel very accomplished making this zodiac wheel. It lives in the ether, beyond me and anyone else. For this wheel
+                                is above us. This is not my opinion, it is truth. Aligned with the truth, and contributing to the Great Work. 
+                            </p>
                           
                             <p>
                                 Amen. 
-                            </p> */}
+                            </p>
                         </div>
                     </div>
             </div>
--- project-trinity/src/components/Zodiac.js
+++ project-trinity/src/components/Zodiac.js
@@ -7,6 +7,24 @@ import taurusdesktop from '../assets/zodiacs/cards/taurus-card-desktop.svg'
 import taurusCard from '../assets/zodiacs/cards/taurus-card.svg'
 import geminidesktop from '../assets/zodiacs/cards/gemini-card-desktop.svg'
 import geminiCard from '../assets/zodiacs/cards/gemini-card.svg'
+import cancerdesktop from '../assets/zodiacs/cards/cancer-card-desktop.svg'
+import cancerCard from '../assets/zodiacs/cards/cancer-card.svg'
+import leodesktop from '../assets/zodiacs/cards/leo-card-desktop.svg'
+import leoCard from '../assets/zodiacs/cards/leo-card.svg'
+import virgodesktop from '../assets/zodiacs/cards/virgo-card-desktop.svg'
+import virgoCard from '../assets/zodiacs/cards/virgo-card.svg'
+import libradesktop from '../assets/zodiacs/cards/libra-card-desktop.svg'
+import libraCard from '../assets/zodiacs/cards/libra-card.svg'
+import scorpiodesktop from '../assets/zodiacs/cards/scorpio-card-desktop.svg'
+import scorpioCard from '../assets/zodiacs/cards/scorpio-card.svg'
+import sagittariusdesktop from '../assets/zodiacs/cards/sagittarius-card-desktop.svg'
+import sagittariusCard from '../assets/zodiacs/cards/sagittarius-card.svg'
+import capricorndesktop from '../assets/zodiacs/cards/capricorn-card-desktop.svg'
+import capricornCard from '../assets/zodiacs/cards/capricorn-card.svg'
+import aquariusdesktop from '../assets/zodiacs/cards/aquarius-card-desktop.svg'
+import aquariusCard from '../assets/zodiacs/cards/aquarius-card.svg'
+import piscesdesktop from '../assets/zodiacs/cards/pisces-card-desktop.svg'
+import piscesCard from '../assets/zodiacs/cards/pisces-card.svg'
 import taurus from '../assets/zodiacs/taurus.svg'
 import gemini from '../assets/zodiacs/gemini.svg'
 import cancer from '../assets/zodiacs/cancer.svg'
@@ -38,11 +56,50 @@ function Zodiac() {
    const handleGeminiClick = () => {
     setVisibleDiv(visibleDiv === 'Gemini' ? null : 'Gemini');
   };
+    // Function to handle Cancer click
+    const handleCancerClick = () => {
+    setVisibleDiv(visibleDiv === 'Cancer' ? null : 'Cancer');
+  };
 
+    // Function to handle Leo click
+    const handleLeoClick = () => {
+    setVisibleDiv(visibleDiv === 'Leo' ? null : 'Leo');
+  };
+
+    // Function to handle Virgo click
+    const handleVirgoClick = () => {
+    setVisibleDiv(visibleDiv === 'Virgo' ? null : 'Virgo');
+  };
+  // Function to handle Libra click
+  const handleLibraClick = () => {
+    setVisibleDiv(visibleDiv === 'Libra' ? null : 'Libra');
+  };
+    // Function to handle Scorpio click
+    const handleScorpioClick = () => {
+    setVisibleDiv(visibleDiv === 'Scorpio' ? null : 'Scorpio');
+  };
+  // Function to handle Sagittarius click
+  const handleSagittariusClick = () => {
+    setVisibleDiv(visibleDiv === 'Sagittarius' ? null : 'Sagittarius');
+  };
+
+  // Function to handle Capricorn click
+  const handleCapricornClick = () => {
+    setVisibleDiv(visibleDiv === 'Capricorn' ? null : 'Capricorn');
+  };
+
+  // Function to handle Aquarius click
+  const handleAquariusClick = () => {
+    setVisibleDiv(visibleDiv === 'Aquarius' ? null : 'Aquarius');
+  };
+
+    // Function to handle Pisces click
+  const handlePiscesClick = () => {
+    setVisibleDiv(visibleDiv === 'Pisces' ? null : 'Pisces');
+  };
     return (
-      <div className='zodiac-main-container'>
-        
 
+      <div className='zodiac-main-container'>
         {visibleDiv === 'Aries' && (
         <motion.div 
           style={{ margin: '0px', padding: '20px' }}
@@ -71,8 +128,111 @@ function Zodiac() {
           animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
           transition={{ duration: 1 }}
           style={{ margin: '0px', padding: '20px' }}>
-          <img className="desktop-card" src={geminidesktop} alt="taurus description" />
-          <img className="card" src={geminiCard} alt="taurus description" />
+          <img className="desktop-card" src={geminidesktop} alt="gemini description" />
+          <img className="card" src={geminiCard} alt="gemini description" />
+
+        </motion.div>
+      )}
+      {visibleDiv === 'Cancer' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={cancerdesktop} alt="Cancer description" />
+          <img className="card" src={cancerCard} alt="Cancer description" />
+
+        </motion.div>
+      )}
+       {visibleDiv === 'Leo' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={leodesktop} alt="Leo description" />
+          <img className="card" src={leoCard} alt="Leo description" />
+
+        </motion.div>
+      )}
+        {visibleDiv === 'Virgo' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={virgodesktop} alt="Virgo description" />
+          <img className="card" src={virgoCard} alt="Virgo description" />
+
+        </motion.div>
+      )}
+        {visibleDiv === 'Libra' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={libradesktop} alt="Libra description" />
+          <img className="card" src={libraCard} alt="Libra description" />
+
+        </motion.div>
+      )}
+
+{visibleDiv === 'Scorpio' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={scorpiodesktop} alt="Scorpio description" />
+          <img className="card" src={scorpioCard} alt="Scorpio description" />
+
+        </motion.div>
+      )}
+
+{visibleDiv === 'Sagittarius' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={sagittariusdesktop} alt="Sagittarius description" />
+          <img className="card" src={sagittariusCard} alt="Sagittarius description" />
+
+        </motion.div>
+      )}
+
+      {visibleDiv === 'Capricorn' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={capricorndesktop} alt="Capricorn description" />
+          <img className="card" src={capricornCard} alt="Capricorn description" />
+
+        </motion.div>
+      )}
+
+      {visibleDiv === 'Aquarius' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={aquariusdesktop} alt="Aquarius description" />
+          <img className="card" src={aquariusCard} alt="Aquarius description" />
+
+        </motion.div>
+      )}
+      {visibleDiv === 'Pisces' && (
+        <motion.div 
+          initial={{ y: '1vw', opacity: 0 }} // Initial opacity set to 0
+          animate={{ y: 0, opacity:  1 }} // Animate opacity to 1
+          transition={{ duration: 1 }}
+          style={{ margin: '0px', padding: '20px' }}>
+          <img className="desktop-card" src={piscesdesktop} alt="Pisces description" />
+          <img className="card" src={piscesCard} alt="Pisces description" />
 
         </motion.div>
       )}
@@ -85,16 +245,16 @@ function Zodiac() {
           class="zodiac-wheel">
           <div onClick={handleAriesClick} class="slice aries"><span><img  src={aries} alt="aries"/></span></div>
           <div onClick={handleTaurusClick} class="slice taurus"><span><img src={taurus} alt="taurus"/></span></div>
-          <div onClick={handleGeminiClick}class="slice gemini"><span><img src={gemini} alt="gemini"/></span></div>
-          <div class="slice cancer"><span><img src={cancer} alt="cancer"/></span></div>
-          <div class="slice leo"><span><img src={leo} alt="leo"/></span></div>
-          <div class="slice virgo"><span><img src={virgo} alt="virgo"/></span></div>
-          <div class="slice libra"><span><img src={libra} alt="libra"/></span></div>
-          <div class="slice scorpio"><span><img src={scorpio} alt="scorpio"/></span></div>
-          <div class="slice sagittarius"><span><img src={sagittarius} alt="sagittarius"/></span></div>
-          <div class="slice capricorn"><span><img src={capricorn} alt="capricorn"/></span></div>
-          <div class="slice aquarius"><span><img src={aquarius} alt="aquarius"/></span></div>
-          <div class="slice pisces"><span><img src={pisces} alt="pisces"/></span></div>
+          <div onClick={handleGeminiClick} class="slice gemini"><span><img src={gemini} alt="gemini"/></span></div>
+          <div onClick={handleCancerClick} class="slice cancer"><span><img src={cancer} alt="cancer"/></span></div>
+          <div onClick={handleLeoClick} class="slice leo"><span><img src={leo} alt="leo"/></span></div>
+          <div onClick={handleVirgoClick} class="slice virgo"><span><img src={virgo} alt="virgo"/></span></div>
+          <div onClick={handleLibraClick} class="slice libra"><span><img src={libra} alt="libra"/></span></div>
+          <div onClick={handleScorpioClick} class="slice scorpio"><span><img src={scorpio} alt="scorpio"/></span></div>
+          <div onClick={handleSagittariusClick} class="slice sagittarius"><span><img src={sagittarius} alt="sagittarius"/></span></div>
+          <div onClick={handleCapricornClick} class="slice capricorn"><span><img src={capricorn} alt="capricorn"/></span></div>
+          <div onClick={handleAquariusClick} class="slice aquarius"><span><img src={aquarius} alt="aquarius"/></span></div>
+          <div onClick={handlePiscesClick} class="slice pisces"><span><img src={pisces} alt="pisces"/></span></div>
         </motion.div>
       </div>
       
