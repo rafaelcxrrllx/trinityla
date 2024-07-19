678fd7c3dba7e28f3cc67b5af299ebfaad9a934e
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-07-19 11:54:54 -0700

    Changed px size

--- project-trinity/src/components/Nav.js
+++ project-trinity/src/components/Nav.js
@@ -61,9 +61,9 @@ useEffect(() => {
         <motion.div 
         id="menu"
         initial={{ height: 0, opacity: 0 }}
-        animate={menuOpen ? { height: '100%', opacity: 1 } : { height: 0, opacity: 0 }}
+        animate={menuOpen ? { height: '500px', opacity: 1 } : { height: 0, opacity: 0 }}
         transition={{ duration: .3 }}
-        style={{ overflow: 'hidden', position: 'fixed', backgroundColor: '#070F19' }}>
+        style={{ position: 'fixed', backgroundColor: '#070F19' }}>
         <div className='flourescent-nav'></div>
 
         <motion.ul 
